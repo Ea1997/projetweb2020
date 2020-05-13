95f42c1b90f56fad3340ef6007dcc2cea98df3fb
unknown <salim.elazhari@gmail.com>
2020-05-13 20:40:05 +0000

    first commit

---
+++ app/Profile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+
+    protected $guarded = [];
+    public function profileImage()
+    {
+      $imagepath=($this->image)   ? $this->image : 'profile/none.jpg';
+return  '/storage/' . $imagepath ;
+   }
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
+
+}
